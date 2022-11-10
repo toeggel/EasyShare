d96da4f34c6c02d247c894a50895421bc544211b
Martin Weber <weber_martin@gmx.ch>
2022-11-10 17:25:50 +0100

    fix SSR

--- frontend/src/routes/[id]/+page.ts
+++ frontend/src/routes/[id]/+page.ts
@@ -1,10 +1,13 @@
 import type { PageLoad } from './$types';
 import type { SharedContent } from '$lib/_generated-api';
 
-// disable SSR (because the fetch fails otherwise.. why?)
-export const ssr = true
+// Locally we struggle with self-signed certificate.
+// We can disable it by running next line (once) (it can be enabled with "=1".
+// process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = 0
+// Or disable SSR all together
+// export const ssr = true
 
 export const load: PageLoad = async ({ fetch, params }) => {
-    const res = await fetch(`https://127.0.0.1:7200/shared-contents/${ params.id }`);
+    const res = await fetch(`https://localhost:7200/shared-contents/${ params.id }`);
     return await res.json() as SharedContent;
 };
